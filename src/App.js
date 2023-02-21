132a75e14fe7fc7c3df09026c8360f68b9bbed70
Agailh <agailham86@gmail.com>
2023-02-21 11:47:50 +0700

    about init

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import About from "./component/About";
 import Hero from "./component/Hero";
 import Navbar from "./component/Navbar";
 
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Hero />
+      <About />
     </div>
   );
 }
---
+++ src/component/About.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About = () => {
+  return <div className="max-w-[1240px] mx-auto py-16 px-4 text-center">About</div>;
+};
+
+export default About;
