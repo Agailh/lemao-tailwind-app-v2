8fdfd58e53a13dd9b83c13998262fb21a51b164c
Agailh <agailham86@gmail.com>
2023-03-01 16:46:21 +0700

    commit

--- src/component/VideoList.jsx
+++ src/component/VideoList.jsx
@@ -17,13 +17,13 @@ const VideoList = () => {
   return (
     <div className="w-full h-full relative bg-gray-800">
       <h1 className="text-center text-white py-10">Newest and Upcoming Kaela's stream </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 ">
+      <div className="px-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 ">
         {videos.map((video) => (
           <div className="h-full flex justify-center items-center">
-            <div className="border rounded-md text-white p-4 cursor-pointer" key={video.id.videoId}>
+            <div className="border hover:bg-gray-500 rounded-md text-white p-4 cursor-pointer" key={video.id.videoId}>
               <a href={`https://www.youtube.com/watch?v=${video.id.videoId}`} target="_blank" rel="noopener noreferrer">
                 <img className="mx-auto my-auto" src={video.snippet.thumbnails.medium.url} alt={video.snippet.title} />
-                <h3 className="text-center text-sm">{video.snippet.title}</h3>
+                <h3 className="text-center text-sm text-semibold py-5">{video.snippet.title}</h3>
               </a>
             </div>
           </div>
