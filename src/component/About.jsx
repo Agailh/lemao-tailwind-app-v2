b5eb40b452298a900abbb86970f3f1d97ccf6f11
Agailh <agailham86@gmail.com>
2023-03-01 16:51:07 +0700

    commit

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -26,7 +26,7 @@ const About = () => {
       <div className="absolute w-full top-0 h-full flex flex-col justify-center text-center p-4 ">
         <div className="flex flex-col justify-between items-center max-w-[800px] mx-auto w-full border p-1 rounded-md text-white ">
           <img className="h-[200px] w-[200px] rounded-full" src={channel.snippet.thumbnails.medium.url} alt={channel.snippet.title} />
-          <h1 className="py-5">{channel.snippet.title}</h1>
+          <h1 className="py-5">Kaela Kovalskia</h1>
           <div className="flex justify-between items-center gap-1 py-5">
             <h2>
               The <span className="line-through">Cutest</span> Coolest Blacksmith from Hololive Indonesia Gen 3
@@ -37,7 +37,7 @@ const About = () => {
           <h3 className="py-5">Fan Name : Pemaloe </h3>
           <div className="flex justify-between items-center gap-2">
             <FaYoutube className="text-3xl text-[#FF0000]" />
-            <h3>Kaela Kovalskia Ch. hololive-ID</h3>
+            <h3>{channel.snippet.title}</h3>
           </div>
         </div>
       </div>
